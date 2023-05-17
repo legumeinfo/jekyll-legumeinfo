59722806b59fda875e1a2209673fa423640006f2
Sam Hokin <shokin@ncgr.org>
2023-05-17 13:22:24 -0600

    Temp commit for @adf-ncgr DO NOT PR THIS BRANCH

--- tools/search/js/graphql.js
+++ tools/search/js/graphql.js
@@ -1,8 +1,11 @@
 // query a local instance of the LIS GraphQL server:
 // https://github.com/legumeinfo/graphql-server
 
-// Note: using port 4444 since port 4000 is default for jekyll site
-const uri = 'http://localhost:4444/';
+// should put graphqluri in _config.yml but I can't figure out how to do it here! /Sam
+const uri = 'https://jekyll-dev.lis.ncgr.org/graphql' 
+
+// dev
+// const uri = 'http://localhost:4444/';
 
 // A function that gets data from a GraphQL server via a POST request.
 // Adapted from https://graphql.org/graphql-js/graphql-clients/
@@ -18,24 +21,25 @@ function graphqlQuery(uri, query, variables={}, abortSignal=undefined) {
             variables,
         }),
         signal: abortSignal,
-    }).then(r => r.json());
+    })
+    .then(r => r.json())
+    .then((response) => {
+      if (response.errors) throw response.errors;
+      return response;
+    });
 }
 // TODO: implement error handling
 
 // Flatten GraphQL results that contain objects
 const flatten = (obj, out={}, prefixes=[]) => {
-    if (obj != null && typeof obj == 'object' && !Array.isArray(obj)) {
+    if (obj != null) {
         Object.keys(obj).forEach(key => {
             const key_prefixes = [...prefixes, key];
             if (typeof obj[key] == 'object' && !Array.isArray(obj[key])) {
                 out = flatten(obj[key], out, key_prefixes);
             } else {
                 const prefix_key = key_prefixes.join('_');
-                if (Array.isArray(obj[key])) {
-                    out[prefix_key] = obj[key].map((value) => flatten(value));
-                } else {
-                    out[prefix_key] = obj[key];
-                }
+                out[prefix_key] = obj[key];
             }
         });
     }
